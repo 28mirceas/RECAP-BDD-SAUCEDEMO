464dd9af5bad37dc3bd768eb2379e97b682d86a8
28mirceas <mir.sava@gmail.com>
2026-09-14 21:06:00 +0300

    Login Page commit

--- pages/login_page.py
+++ pages/login_page.py
@@ -34,7 +34,7 @@ class LoginPage(BasePage):
 
     def verify_login_error_message(self, expected_message):
         actual_message = self.get_login_error_message_text()
-
+        print("Modificare pentru testare GIT!")
         assert actual_message == expected_message, (
             f"Expected error: {expected_message}, "
             f"but got: {actual_message}"
